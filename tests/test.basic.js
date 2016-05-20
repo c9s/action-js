38741fabbbc36dba9f61149bda63a9ee194f426a
c9s <yoanlin93@gmail.com>
2016-05-20 17:25:33 +0800

    Update test case

--- tests/test.basic.js
+++ tests/test.basic.js
@@ -1,15 +1,9 @@
 console.clear();
-describe('retries', function() {
-  it('should succeed on the 3rd try', function () {
-
+describe('Simple Action', function() {
+  it('should return success response', function () {
     Action.ajaxOptions.url = "/api";
     runAction('TestApp::Action::Simple', {}, function(resp) {
       expect(resp.success).to.be.true;
     });
-    /*
-    $.getJSON('/api', { __action: 'Foo' }, function(resp) {
-      console.log(resp);
-    });
-    */
   });
 });
